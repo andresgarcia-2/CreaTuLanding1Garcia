7b7751efd264973aab1dc56d656896eb5a8699ed
andres garcia <andresagustingp@gmail.com>
2025-08-31 21:35:07 -0300

    componente CartWidget hecho

---
+++ src/componentes/CartWidget.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const CartWidget = () => {
+    return (
+    <div className="cart-widget">
+        <i className="fas fa-clipboard-list"></i>
+        <span className="cart-count">2</span>
+    </div>
+    );
+};
