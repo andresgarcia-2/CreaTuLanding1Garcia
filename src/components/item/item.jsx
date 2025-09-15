26875c2d9f14f7e4817b4f03c3344d61e0ae246b
andres garcia <andresagustingp@gmail.com>
2025-09-15 17:48:34 -0300

    puse product que recibe un producto como prop, el link y para que el precio tenga puntos

---
+++ src/components/item/item.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Item = ({ product }) => {
+    return (
+        <div className="item-card">
+            <img src={product.image} alt={product.name} />
+            <div className="item-info">
+                <h3>{product.name}</h3>
+                <p className="item-price">${product.price.toLocaleString()}</p>
+                <p className="item-description">{product.description}</p>
+                <Link to={`/item/${product.id}`} className="btn-detail">
+                    Ver Detalle
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Item;
